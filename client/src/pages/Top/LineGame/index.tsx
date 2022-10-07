30a7a6f911e10697dc2cc4ba93574ac029f1721f
hibohiboo <swdice66@gmail.com>
2022-10-08 08:47:29 +0900

    add フィールドの描画

--- client/src/pages/Top/LineGame/index.tsx
+++ client/src/pages/Top/LineGame/index.tsx
@@ -1,11 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 
-const StyledWrapper = styled.div`
-  padding: 0px;
+const StyledWrapper = styled.div``
+const StyledConsole = styled.div`
+  width: 640px;
+  height: 480px;
+  border: solid 2px #eee;
+  padding: 10px;
+
+  /* レトロゲームコンソール風の文字に */
+  font-family: 'DotGothic16', sans-serif;
+  white-space: pre-wrap;
+  font-size: 16px;
+  line-height: 1;
+
+  /* 単純な描画領域として扱うため余分なイベントを無効化 */
+  pointer-events: none;
+  user-select: none;
 `
 
 const LifeGame: React.FC = () => {
-  return <StyledWrapper>lifegame</StyledWrapper>
+  const hooks = useHooks()
+  return (
+    <StyledWrapper>
+      <StyledConsole>{hooks.state}</StyledConsole>
+      <button onClick={hooks.nextState}> 次の世代</button>
+    </StyledWrapper>
+  )
 }
 export default LifeGame
+
+const useHooks = () => {
+  const [gen] = useState(mainLoop())
+  const [state, setState] = useState<string>(gen.next().value || '')
+  const nextState = () => {
+    const nextState = gen.next().value
+    console.log(nextState)
+    if (nextState) setState(nextState)
+  }
+
+  return { state, nextState }
+}
+
+function* mainLoop() {
+  while (true) {
+    yield drawField()
+  }
+}
+const FIELD_WIDTH = 3
+const FIELD_HEIGHT = 3
+const field = [
+  [0, 1, 0],
+  [0, 0, 1],
+  [1, 1, 1],
+]
+function drawField() {
+  let ret = ''
+  for (let y = 0; y < FIELD_HEIGHT; y++) {
+    for (let x = 0; x < FIELD_WIDTH; x++) {
+      ret = `${ret}${field[y][x] ? '■' : '　'}`
+    }
+    ret = `${ret}${'\n'}`
+  }
+  return ret
+}
--- client/src/pages/Top/index.tsx
+++ client/src/pages/Top/index.tsx
@@ -4,14 +4,39 @@ import BattleScene from './BattleScene'
 import LifeGame from './LineGame'
 
 const StyledWrapper = styled.div`
-  padding: 0px;
+  padding-bottom: 50px;
 `
-
+const StyledSection = styled.section`
+  h1 {
+    font-size: 1.2rem;
+  }
+  h2 {
+    font-size: 1rem;
+  }
+  max-width: 400px;
+`
 const Top: React.FC = () => {
   return (
     <StyledWrapper>
-      <BattleScene />
-      <LifeGame />
+      <StyledSection>
+        <h1>【第1章】RPGの戦闘シーン</h1>
+        <h2>回復呪文を軸とした、ラスボスとの激闘！</h2>
+        <p>
+          圧勝する勇者。しかしそこに降臨した魔王に歯が立たず、「にげる」コマンドを実装して逃げてしまう…
+          このまま引き下がれないと、「じゅもん」コマンドを実装し、再び魔王に挑むが果たして…
+          勇者の運命や如何に？！
+        </p>
+        <BattleScene />
+      </StyledSection>
+
+      <StyledSection>
+        <h1>【第2章】ライフゲーム</h1>
+        <h2>神秘的な生命シミュレーション</h2>
+        <p>
+          生命の「繁殖」、過疎や過密による「死滅」を再現する、見て楽しむシミュレーションゲームです。世代を進める方法として、キーボードを押す毎に進める方法と、リアルタイムに自動で進める方法を実装します。任意のパターンを、フィールドの任意の場所にコピーする処理も実装します。本章の最後では、世界中で発見された興味深いパターンを紹介します。
+        </p>
+        <LifeGame />
+      </StyledSection>
     </StyledWrapper>
   )
 }
