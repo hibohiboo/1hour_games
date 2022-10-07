69a7a0aae12f7e1d6f307150391ffe2eb609a326
hibohiboo <swdice66@gmail.com>
2022-10-08 08:13:09 +0900

    add ライフゲーム枠作成

---
+++ client/src/pages/Top/LineGame/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledWrapper = styled.div`
+  padding: 0px;
+`
+
+const LifeGame: React.FC = () => {
+  return <StyledWrapper>lifegame</StyledWrapper>
+}
+export default LifeGame
